f6f2a77945ebc27dc83db468822818bc2bb9e409
lsj2190 <deneb219@naver.com>
2020-12-30 17:42:39 +0900

    fix date, stop when the date is reached

--- timer/script.js
+++ timer/script.js
@@ -1,8 +1,14 @@
-var expected_date = new Date("2020-10-31");
+var expected_date = new Date("2021-10-31");
+var print_t = 0;
 
-window.setInterval(printTime,1000);
+print_t = window.setInterval(printTime,1000);
 
 function printTime() {
+  if(expected_date.getTime()-Date.now()<0)
+  {
+    clearInterval(print_t);
+    return;
+  }
   document.getElementById("days").innerHTML=Math.floor((expected_date.getTime()-Date.now())/1000/60/60/24);
   document.getElementById("hours").innerHTML=Math.floor((expected_date.getTime()-Date.now())/1000/60/60%24);
   document.getElementById("minutes").innerHTML=Math.floor((expected_date.getTime()-Date.now())/1000/60%60);
